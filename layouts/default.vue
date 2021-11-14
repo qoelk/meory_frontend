4ab75bec3cb559bd0eef46dcefb4e1c8bef946ef
qoelk <qoelky@gmail.com>
2021-11-14 18:38:18 +0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+//
+</template>
+
+<script>
+export default {
+  name: "default.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="">hi</div>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style scoped>
+
+</style>
